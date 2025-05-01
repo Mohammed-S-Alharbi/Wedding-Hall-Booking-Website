8c796b6a33e6844016fb2483a0b25653ad09c8b5
khalidbawidan <khalid.bawidan@gmail.com>
2025-05-01 23:03:21 +0300

    complete

---
+++ WeddingHallBooking/WeddingHallBooking/Controllers/AccountController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeddingHallBooking.Models;
+using WeddingHallBooking.Data;
+
+namespace WeddingHallBooking.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Account/Register
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(User model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Save user to DB
+                _context.Users.Add(model);
+                _context.SaveChanges();
+
+                // Redirect to login or homepage
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
+        }
+
+        // GET: /Account/Login
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(string email, string password)
+        {
+            var user = _context.Users.FirstOrDefault(u => u.Email == email && u.Password == password);
+            if (user != null)
+            {
+                // Set session (or cookie)
+                HttpContext.Session.SetString("UserName", user.FullName);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Invalid email or password";
+            return View();
+        }
+
+        // GET: /Account/Logout
+        public IActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ WeddingHallBooking/WeddingHallBooking/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WeddingHallBooking.Models;
+
+namespace WeddingHallBooking.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+
+    }
+}
